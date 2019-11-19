3c011cb44a817fd93feb1a9ebbb5bb1a0bb281b8
Exybore <exybore@becauseofprog.fr>
2019-11-19 19:58:42 +0100

    chore(dependencies): add vue-router

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
