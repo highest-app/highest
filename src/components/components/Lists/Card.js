de15aa05b23aa7ecc5251f4d8de2ae7316e54220
Exybore <exybore@becauseofprog.fr>
2020-01-21 22:04:38 +0100

    feat: improve card component
    Improved the Card component, by adding props to control borders and new
    slots, for inputs and action texts. Also imported styles from the
    SelectMenu.

--- src/components/components/Lists/Card.js
+++ src/components/components/Lists/Card.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 
 export default Vue.component('card', {
     props: {
+      bottom: {
+        type: Boolean,
+        default: false
+      },
+      class: {
+        type: String,
+        default: ''
+      },
       clickable: {
         type: Boolean,
         default: false
@@ -14,21 +22,25 @@ export default Vue.component('card', {
         type: String,
         default: null
       },
+      top: {
+        type: Boolean,
+        default: false
+      }
     },
     render(createElement) {
       return createElement(
         'div', [
           createElement(
-            'v-list',
-            {
+            'v-list', {
               class: {
-                'card': true
+                'card': true,
+                'card--top': this.top,
+                'card--bottom': this.bottom
               }
             },
             [
               createElement(
-                'v-list-item',
-                {
+                'v-list-item', {
                   props: {
                     link: this.clickable,
                     href: this.href,
@@ -36,19 +48,42 @@ export default Vue.component('card', {
                   }
                 },
                 [
-                  createElement('v-list-content', [
-                    createElement('v-list-title', this.$slots.title)
-                  ]),
-                  createElement('v-spacer'),
-                  this.$slots.action
+                  createElement(
+                    'v-row', {
+                      class: {
+                        'mx-0': true
+                      },
+                      props: {
+                        align: 'center'
+                      }
+                    },
+                    [
+                      createElement(
+                        'span', {
+                          class: { 'subtitle-1': true}
+                        },
+                        this.$slots.title
+                      ),
+                      this.$slots.input,
+                      createElement('v-spacer'),
+                      createElement(
+                        'span', {
+                          class: {
+                            'subtitle-1': true,
+                            'action__text': true
+                          },
+                        }, this.$slots['action-text']
+                      ),
+                      this.$slots.action
+                    ]
+                  ),
                 ]
               ),
-              createElement('v-divider')
+              this.bottom ? '' : createElement('v-divider')
             ]
           ),
           this.$slots.description !== undefined ? createElement(
-            'p',
-            {
+            'p', {
               class: {
                 'card__description': true
               }
