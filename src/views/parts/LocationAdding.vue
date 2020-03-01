858b9033da2d4a227fa64e0b5b51231e87be8550
Exybore <exybore@becauseofprog.fr>
2020-03-01 18:58:49 +0100

    fix: fix background of location dialog

--- src/views/parts/LocationAdding.vue
+++ src/views/parts/LocationAdding.vue
@@ -1,63 +1,67 @@
 <template>
-  <div class="location-adding-menu">
-    <app-bar
-      :title="$t('locations.add')"
-      small-only
-      fixed>
-      <template #bar-left-actions>
-        <a @click="close">{{ $t('terms.cancel') }}</a>
-      </template>
-      <template #bar-right-actions>
-        <a @click="add">{{ $t('terms.add') }}</a>
-      </template>
-    </app-bar>
-    <page-body>
-      <list-group>
-        <card top>
-          <template #title>{{ $t('terms.name') }}</template>
-          <template #input>
-            <v-text-field
-              v-model="form.name"
-              :placeholder="$t('locations.namePlaceholder')"
-              hide-details
-              solo
-              flat/>
-          </template>
-        </card>
-        <card>
-          <template #title>{{ $t('terms.picture') }}</template>
-          <template #input>
-            <v-text-field
-              v-model="form.picture"
-              :placeholder="$t('terms.picturePlaceholder')"
-              hide-details
-              solo
-              flat/>
-          </template>
-          <template #description>
-            {{ $t('terms.pictureDescription') }}
-            <a href="https://imgur.com">Imgur</a>
-          </template>
-        </card>
-        <card
-          top
-          bottom>
-          <template #title>{{ $t('terms.notes') }}</template>
-          <template #input>
-            <v-textarea
-              id="notes-textarea"
-              v-model="form.notes"
-              :placeholder="$t('locations.notesPlaceholder')"
-              rows="1"
-              auto-grow
-              hide-details
-              solo
-              flat/>
-          </template>
-        </card>
-      </list-group>
-    </page-body>
-  </div>
+  <v-card class="background">
+    <v-card-text
+      class="location-adding-menu pa-0"
+      style="overflow-x: hidden;">
+      <app-bar
+        :title="$t('locations.add')"
+        small-only
+        fixed>
+        <template #bar-left-actions>
+          <a @click="close">{{ $t('terms.cancel') }}</a>
+        </template>
+        <template #bar-right-actions>
+          <a @click="add">{{ $t('terms.add') }}</a>
+        </template>
+      </app-bar>
+      <page-body>
+        <list-group>
+          <card top>
+            <template #title>{{ $t('terms.name') }}</template>
+            <template #input>
+              <v-text-field
+                v-model="form.name"
+                :placeholder="$t('locations.namePlaceholder')"
+                hide-details
+                solo
+                flat/>
+            </template>
+          </card>
+          <card>
+            <template #title>{{ $t('terms.picture') }}</template>
+            <template #input>
+              <v-text-field
+                v-model="form.picture"
+                :placeholder="$t('terms.picturePlaceholder')"
+                hide-details
+                solo
+                flat/>
+            </template>
+            <template #description>
+              {{ $t('terms.pictureDescription') }}
+              <a href="https://imgur.com">Imgur</a>
+            </template>
+          </card>
+          <card
+            top
+            bottom>
+            <template #title>{{ $t('terms.notes') }}</template>
+            <template #input>
+              <v-textarea
+                id="notes-textarea"
+                v-model="form.notes"
+                :placeholder="$t('locations.notesPlaceholder')"
+                rows="1"
+                auto-grow
+                hide-details
+                solo
+                flat/>
+            </template>
+          </card>
+        </list-group>
+      </page-body>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
