fc61d35dd8125113673cb4b81f090265b4f771b2
Exybore <exybore@becauseofprog.fr>
2020-03-14 23:00:18 +0100

    wip: add map for competitions

--- src/views/Competition.vue
+++ src/views/Competition.vue
@@ -4,37 +4,57 @@
       :title="competition.name"
       small-only>
       <template #bar-left-actions>
-        <a @click="$router.back()">{{ $t('terms.return') }}</a>
+        <a @click="$router.back()">{{ $t('terms.back') }}</a>
       </template>
       <template #bar-right-actions>
         <a>{{ $t('terms.edit') }}</a>
       </template>
     </app-bar>
-    <page-body>
-      <a
-        v-if="location.type === 'external'"
-        :href="location.link"
-        target="_blank">
-        {{ location.name }}
-      </a>
-      <router-link
-        v-if="location.type === 'internal'"
-        :to="location.link">
-        {{ location.name }}
-      </router-link>
-    </page-body>
+    <v-container>
+      <v-row>
+        <v-col
+          style="height: 500px"
+          cols="12"
+          md="6">
+          <l-map
+            v-if="showMap"
+            :zoom="15"
+            :center="[mapInfo['y'], mapInfo['x']]">
+            <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            <l-marker :lat-lng="latLng(mapInfo['y'], mapInfo['x'])"/>
+          </l-map>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6">
+          <h1>{{ competition.name }}</h1>
+          <p>{{ competition.description }}</p>
+          <span class="primary--text">{{ dateToText(competition.date) }}</span>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-content>
 </template>
 
 <script>
+import { latLng } from 'leaflet'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import { OpenStreetMapProvider } from 'leaflet-geosearch'
 import { mapGetters } from 'vuex'
+import { dateToText } from '@/utils/parsing'
+
+const provider = new OpenStreetMapProvider()
 
 export default {
   name: 'Competition',
+  components: { LMap, LTileLayer, LMarker },
   data () {
     return {
       competition: {},
-      location: {}
+      location: {},
+
+      showMap: false,
+      mapInfo: {}
     }
   },
   mounted () {
@@ -45,6 +65,10 @@ export default {
         name: this.competition.location.value,
         link: new URL(`https://www.openstreetmap.org/search?query=${this.competition.location.value}`)
       }
+      provider.search({ query: this.competition.location.value }).then(response => {
+        this.mapInfo = response[0]
+        this.showMap = true
+      })
     } else if (this.competition.location.type === 'location') {
       const internalLocation = this.getLocationById(this.competition.location.value)
       this.location = {
@@ -56,6 +80,10 @@ export default {
   },
   computed: {
     ...mapGetters(['getCompetitionById', 'getLocationById'])
+  },
+  methods: {
+    latLng,
+    dateToText
   }
 }
 </script>
