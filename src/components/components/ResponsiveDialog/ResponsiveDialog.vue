41809c1a38019a7dbf9af79aba29ef87681550fc
Exybore <exybore@becauseofprog.fr>
2020-04-12 23:31:33 +0200

    refactor: add better active state handling for responsive dialog

--- src/components/components/ResponsiveDialog/ResponsiveDialog.vue
+++ src/components/components/ResponsiveDialog/ResponsiveDialog.vue
@@ -2,7 +2,7 @@
   <div class="responsive-dialog">
     <template v-if="$vuetify.breakpoint.mdAndUp">
       <v-dialog
-        v-model="value"
+        v-model="active"
         :max-width="dialogWidth"
         class="responsive-dialog__dialog">
         <v-card class="background">
@@ -14,43 +14,67 @@
         </v-card>
       </v-dialog>
       <div
-        class="responsive-dialog__trigger"
-        @click="value = true">
-        <slot name="activator"/>
+        class="responsive-dialog__trigger">
+        <slot
+          name="activator"
+          v-bind:on="events"/>
       </div>
     </template>
-    <v-bottom-sheet
-      v-else
-      v-model="value"
-      class="responsive-dialog__sheet"
-      scrollable
-      inset>
-      <template #activator="{ on }">
-        <div
-          class="responsive-dialog__trigger"
-          v-on="on">
-          <slot name="activator"/>
-        </div>
-      </template>
-      <v-card class="background">
-        <v-card-text
-          class="location-adding-menu pa-0"
-          style="overflow-x: hidden;">
-          <slot name="dialog"/>
-        </v-card-text>
-      </v-card>
-    </v-bottom-sheet>
+    <template v-else>
+      <v-bottom-sheet
+        v-model="active"
+        class="responsive-dialog__sheet"
+        scrollable
+        inset>
+        <template #activator="{ on }">
+          <div
+            class="responsive-dialog__trigger"
+            v-on="on">
+            <slot
+              name="activator"
+              v-bind:on="events"/>
+          </div>
+        </template>
+        <v-card class="background">
+          <v-card-text
+            class="location-adding-menu pa-0"
+            style="overflow-x: hidden;">
+            <slot name="dialog"/>
+          </v-card-text>
+        </v-card>
+      </v-bottom-sheet>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ResponsiveDialog',
+  model: {
+    prop: 'active',
+    event: 'change'
+  },
   props: {
-    value: Boolean,
+    active: {
+      type: Boolean,
+      default: false
+    },
     dialogWidth: {
       type: String,
       default: '50%'
+    }
+  },
+  data () {
+    return {
+      events: {
+        click: this.activate
+      }
+    }
+  },
+  methods: {
+    activate () {
+      this.active = true
+      this.$emit('change', true)
     }
   }
 }
