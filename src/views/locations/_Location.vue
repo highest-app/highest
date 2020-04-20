1d3695c3b8991718594343282cf3bc9657ac468e
Théo Vidal <exybore@becauseofprog.fr>
2020-04-20 22:38:05 +0200

    feat: add location edition
    Finally!!!

--- src/views/locations/_Location.vue
+++ src/views/locations/_Location.vue
@@ -23,102 +23,136 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <app-bar
-      small-only>
-      <template #bar-left-actions>
-        <a
-          class="hidden-md-and-up"
-          @click="$router.back()">
-          {{ $t('terms.back') }}
-        </a>
-      </template>
-    </app-bar>
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="3">
-          <v-carousel
-            :continuous="false"
-            height="auto"
-            hide-delimiters>
-            <v-carousel-item
-              v-for="photo in location.photos"
-              :key="photo">
-              <v-img
-                :aspect-ratio="16/9"
-                :src="assets[photo]"/>
-            </v-carousel-item>
-          </v-carousel>
-        </v-col>
-        <v-col
-          cols="12"
-          md="9">
-          <v-row>
-            <v-col cols="12">
-              <h1>{{ location.name }}</h1>
-              <span class="overline list-description--text">
-                <v-icon color="list-description">mdi-map-marker-outline</v-icon>
-                {{ location.address }}
-              </span>
-              <p>{{ location.notes }}</p>
-              <v-btn
-                class="mr-3 gradient--primary white--text"
-                depressed
-                @click="editLocation">
-                <v-icon left>mdi-pencil-outline</v-icon>
-                {{ $t('terms.edit') }}
-              </v-btn>
-              <v-btn
-                class="gradient--error white--text"
-                depressed
-                @click="deleteDialog = true">
-                <v-icon left>mdi-delete-outline</v-icon>
-                {{ $t('terms.delete') }}
-              </v-btn>
-            </v-col>
-            <v-col cols="12">
-              <routes-list
-                :routes="routes"
-                flat/>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
+    <template v-if="editMode">
+      <app-bar
+        :title="$t('locations.edit')"
+        small-only>
+        <template #bar-left-actions>
+          <a @click="quitEdit">{{ $t('terms.cancel') }}</a>
+        </template>
+        <template #bar-right-actions>
+          <a @click="validateEdit">{{ $t('terms.ok') }}</a>
+        </template>
+      </app-bar>
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="3"/>
+          <v-col
+            cols="12"
+            md="9">
+            <location-form v-model="form"/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+    <template v-else>
+      <app-bar small-only>
+        <template #bar-left-actions>
+          <a
+            class="hidden-md-and-up"
+            @click="$router.back()">
+            {{ $t('terms.back') }}
+          </a>
+        </template>
+      </app-bar>
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="3">
+            <v-carousel
+              :continuous="false"
+              height="auto"
+              hide-delimiters>
+              <v-carousel-item
+                v-for="photo in location.photos"
+                :key="photo">
+                <v-img
+                  :aspect-ratio="16/9"
+                  :src="assets[photo]"/>
+              </v-carousel-item>
+            </v-carousel>
+          </v-col>
+          <v-col
+            cols="12"
+            md="9">
+            <v-row>
+              <v-col cols="12">
+                <h1>{{ location.name }}</h1>
+                <span class="overline list-description--text">
+                  <v-icon color="list-description">mdi-map-marker-outline</v-icon>
+                  {{ location.address }}
+                </span>
+                <p>{{ location.notes }}</p>
+                <v-btn
+                  class="mr-3 gradient--primary white--text"
+                  depressed
+                  @click="editMode = true">
+                  <v-icon left>mdi-pencil-outline</v-icon>
+                  {{ $t('terms.edit') }}
+                </v-btn>
+                <v-btn
+                  class="gradient--error white--text"
+                  depressed
+                  @click="deleteDialog = true">
+                  <v-icon left>mdi-delete-outline</v-icon>
+                  {{ $t('terms.delete') }}
+                </v-btn>
+              </v-col>
+              <v-col cols="12">
+                <routes-list
+                  :routes="routes"
+                  flat/>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
   </v-content>
 </template>
 
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex'
+import LocationForm from '@/views/locations/LocationForm'
 import RoutesList from '@/views/routes/RoutesList'
 
 export default {
   name: 'Location',
-  components: { RoutesList },
+  components: { LocationForm, RoutesList },
   data: () => ({
     location: {},
     routes: [],
 
+    form: {},
+    editMode: false,
     deleteDialog: false
   }),
   mounted () {
     this.refreshRoutes()
+    this.quitEdit()
   },
   computed: {
     ...mapState(['assets']),
     ...mapGetters(['getLocationById', 'getRoutesByLocation'])
   },
   methods: {
-    ...mapActions(['deleteLocation']),
+    ...mapActions(['updateLocation', 'deleteLocation']),
     refreshRoutes () {
       const id = this.$route.params.location
       this.location = this.getLocationById(id)
       if (this.location === undefined) this.$router.push('/routes')
       else this.routes = this.getRoutesByLocation(this.location.id)
     },
-    editLocation () {
-
+    validateEdit() {
+      this.updateLocation(this.form)
+      this.quitEdit()
+    },
+    quitEdit() {
+      this.form = Object.assign({}, this.location)
+      this.editMode = false
     },
     deleteThis () {
       this.deleteLocation(this.location.id)
