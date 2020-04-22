3eac4f4d628d24cb5fedeb96dd72cc15d2dfc25f
Théo Vidal <exybore@becauseofprog.fr>
2020-04-22 22:44:20 +0200

    refactor: enhance asset uploader
    - New UI to be more comfortable
    - A separated upload button so the 'ok' one doesn't act like this
    - Multiple selection of assets is possible using the property `multiple`, thanks to a new selection system using the item-group component of Vuetify
    
    Removed assets from the locations adding dialog since they're managed in a single place, the location edition menu. Also added a tip to the user to say him that.

--- src/components/components/AssetUploader/AssetUploader.vue
+++ src/components/components/AssetUploader/AssetUploader.vue
@@ -1,73 +1,94 @@
 <template>
-  <responsive-dialog v-model="enabled">
-    <template #activator="{ on }">
-      <card v-on="on">
-        <template #title>{{ $t('terms.picture') }}</template>
-        <template #action-text>{{ $t('terms.select') }}</template>
-        <template #action>
-          <v-list-item-icon v-if="id === ''">
-            <v-icon>mdi-chevron-right</v-icon>
-          </v-list-item-icon>
-          <v-list-item-avatar v-else>
-            <v-img :src="blob"/>
-          </v-list-item-avatar>
-        </template>
-      </card>
-    </template>
-    <template #dialog>
-      <app-bar
-        :title="$t('assets.add')"
-        small-only
-        fixed>
-        <template #bar-right-actions>
-          <a @click="upload">{{ $t('terms.ok') }}</a>
-        </template>
-      </app-bar>
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <strong>{{ $t('assets.uploadHelper') }}</strong>
-            <list-group>
-              <card>
-                <template #title>{{ $t('terms.picture') }}</template>
-                <template #input>
-                  <v-file-input
-                    v-model="file"
-                    :placeholder="$t('terms.picturePlaceholder')"
-                    prepend-icon=""
-                    hide-details
-                    solo
-                    flat
-                    @change="update"/>
-                </template>
-              </card>
-            </list-group>
-            <v-img
-              v-if="uploadBlob !== ''"
-              :src="uploadBlob"/>
-          </v-col>
-          <v-col cols="12">
-            <strong>{{ $t('assets.existingHelper') }}</strong>
-            <v-row>
-              <v-col
-                v-for="(asset, id) in assets"
-                :key="asset"
-                cols="12"
-                md="6"
-                sm="4"
-                lg="3"
-                xl="2">
-                <v-img
-                  v-ripple
-                  :src="asset"
-                  @click="choose(id)"/>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-container>
-    </template>
-  </responsive-dialog>
+  <div class="asset-uploader__container">
+    <slot
+      name="activator"
+      v-bind:on="events"/>
+    <v-slide-x-reverse-transition>
+      <panel
+        v-if="active"
+        :hook="$parent.$refs.content"
+        page>
+        <app-bar
+          :title="$t(title)"
+          small-only
+          fixed>
+          <template #bar-right-actions>
+            <a @click="choose">{{ $t('terms.ok') }}</a>
+          </template>
+        </app-bar>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="6">
+              <strong>{{ $t('assets.uploadHelper') }}</strong>
+              <list-group>
+                <card
+                  top
+                  bottom>
+                  <template #title>{{ $t('terms.picture') }}</template>
+                  <template #input>
+                    <v-file-input
+                      v-model="file"
+                      :placeholder="$t('terms.picturePlaceholder')"
+                      prepend-icon=""
+                      hide-details
+                      solo
+                      flat
+                      @change="updatePreview"/>
+                  </template>
+                </card>
+              </list-group>
+              <template v-if="uploadBlob !== ''">
+                <v-img :src="uploadBlob"/>
+                <v-btn
+                  class="gradient--primary"
+                  block
+                  @click="upload">
+                  Upload
+                </v-btn>
+              </template>
+            </v-col>
+            <v-col
+              cols="12"
+              md="6">
+              <strong>{{ $t('assets.existingHelper') }}</strong>
+              <v-item-group
+                v-model="selectedIds"
+                :multiple="multiple"
+                @change="setSelection">
+                <v-row>
+                  <v-item
+                    v-for="(asset, id) in assets"
+                    :key="asset"
+                    #default="{ active, toggle }">
+                    <v-col
+                      cols="12"
+                      md="6"
+                      sm="4"
+                      lg="3"
+                      @click="toggle">
+                      <v-img
+                        v-ripple
+                        :src="asset"
+                        width="300">
+                        <v-row
+                          class="fill-height ma-0"
+                          align="center"
+                          justify="center">
+                          <v-icon v-if="active">mdi-check-circle-outline</v-icon>
+                        </v-row>
+                      </v-img>
+                    </v-col>
+                  </v-item>
+                </v-row>
+              </v-item-group>
+            </v-col>
+          </v-row>
+        </v-container>
+      </panel>
+    </v-slide-x-reverse-transition>
+  </div>
 </template>
 
 <script>
@@ -76,41 +97,75 @@ import { mapState, mapActions } from 'vuex'
 export default {
   name: 'AssetUploader',
   model: {
-    prop: 'id'
+    prop: 'selectedImages'
   },
   props: {
-    id: {
+    selectedImages: {
+      type: Array,
+      default: () => []
+    },
+    multiple: {
+      type: Boolean,
+      default: false
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    title: {
       type: String,
-      default: ''
-    }
+      default: 'assets.edit'
+    },
   },
   data () {
     return {
       file: '',
       uploadBlob: '',
-      blob: '',
-      enabled: false
+      selectedIds: [],
+
+      events: {
+        click: this.enable
+      }
     }
   },
-  computed: mapState(['assets']),
+  mounted() {
+    for (let value of this.selectedImages) {
+      const keys = Object.keys(this.assets)
+      const index = keys.indexOf(value)
+      this.selectedIds.push(index)
+    }
+  },
+  computed: {
+    ...mapState(['assets'])
+  },
   methods: {
-    ...mapActions(['addAsset']),
-    update () {
+    ...mapActions(['addAsset', 'removeAsset']),
+    enable() {
+      this.active = true
+      this.$emit('change', true)
+    },
+    updatePreview() {
       let reader = new FileReader()
       reader.onload = (e) => {
         this.uploadBlob = e.target.result
       }
       reader.readAsDataURL(this.file)
     },
-    async upload () {
-      let id = await this.addAsset(this.uploadBlob)
-      this.$emit('input', id)
-      this.enabled = false
+    async upload() {
+      await this.addAsset(this.uploadBlob)
+      this.file = ''
+      this.uploadBlob = ''
     },
-    choose(id) {
-      this.$emit('input', id)
-      this.blob = this.assets[id]
-      this.enabled = false
+    setSelection() {
+      this.selectedImages = []
+      this.selectedIds.forEach(id => {
+        const urls = Object.keys(this.assets)
+        this.selectedImages.push(urls[id])
+      })
+    },
+    choose() {
+      this.$emit('input', this.selectedImages)
+      this.active = false
     }
   }
 }
