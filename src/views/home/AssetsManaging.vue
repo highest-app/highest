4172ac2ae9b0f3153a2f8ea24299e4d4406eb7a0
Théo Vidal <exybore@becauseofprog.fr>
2020-12-20 21:19:02 +0100

    fix: fix no assets message in AssetsManaging that was shown even with assets

--- src/views/home/AssetsManaging.vue
+++ src/views/home/AssetsManaging.vue
@@ -2,29 +2,31 @@
   <page-body>
     <p>{{ $t('assets.help.description') }}</p>
     <v-row>
-      <v-col
-        v-for="(asset, id) in assets"
-        :key="asset"
-        cols="12"
-        md="6"
-        sm="4"
-        lg="3">
-        <v-img
-          :src="asset"
-          width="300">
-          <v-row class="mx-1">
-            <v-spacer/>
-            <v-btn
-              :aria-label="$t('assets.remove')"
-              icon
-              @click="() => { removeAsset(id); $forceUpdate() }">
-              <v-icon color="error">mdi-delete-outline</v-icon>
-            </v-btn>
-          </v-row>
-        </v-img>
-      </v-col>
+      <template v-if="Object.keys(assets).length">
+        <v-col
+          v-for="(asset, id) in assets"
+          :key="asset"
+          cols="12"
+          md="6"
+          sm="4"
+          lg="3">
+          <v-img
+            :src="asset"
+            width="300">
+            <v-row class="mx-1">
+              <v-spacer/>
+              <v-btn
+                :aria-label="$t('assets.remove')"
+                icon
+                @click="() => { removeAsset(id); $forceUpdate() }">
+                <v-icon color="error">mdi-delete-outline</v-icon>
+              </v-btn>
+            </v-row>
+          </v-img>
+        </v-col>
+      </template>
       <illustration
-        v-if="!assets.length"
+        v-else
         image="/img/illustrations/image-folder.svg"
         label="assets.any"/>
     </v-row>
